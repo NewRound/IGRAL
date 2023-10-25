14b807d1b46514d62d931c6b0f9831df01612c5d
JuChan Jeong <jjc6252498@gmail.com>
2023-10-25 20:58:42 +0900

    [Update] 회전 클래스 분리

--- Assets/Scripts/Character/Player/PlayerMovement.cs
+++ Assets/Scripts/Character/Player/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Accelerate")]
     [SerializeField] private float acceleratingTime = 10f;
+    private SpeedCalculator _speedCalculator;
     private float _speed;
 
     [Header("Rotation")]
@@ -15,11 +16,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float minAbsAngle = 90f;
     [SerializeField] private float maxAbsAngle = 270f;
     private Vector3 _preDirection;
+    private RotationCalculator _rotationCalculator;
 
     private Vector2 _direction;
     private PlayerController _controller;
 
-    private SpeedCalculator _speedCalculator;
 
     private Rigidbody _rigid;
 
@@ -29,6 +30,7 @@ public class PlayerMovement : MonoBehaviour
         _controller = GetComponent<PlayerController>();
         _rigid = GetComponent<Rigidbody>();
         _speedCalculator = new SpeedCalculator(acceleratingTime);
+        _rotationCalculator = new RotationCalculator(rotationSpeed, minAbsAngle, maxAbsAngle);
     }
 
     private void OnEnable()
@@ -77,24 +79,16 @@ public class PlayerMovement : MonoBehaviour
 
     private void Look()
     {
-
         if (_direction == Vector2.zero)
         {
             transform.rotation = Quaternion.LookRotation(_preDirection);
             return;
         }
 
-        // LookRotation로는 제한을 두기 어렵다고 생각하여 오일러 각도로 변환하여 계산하는 것이 나아보임
+        _preDirection = _direction.x * Vector3.right;
 
-        float currentAngle = transform.rotation.eulerAngles.y;
-        float targetAngle = Vector3.SignedAngle(Vector3.forward, _preDirection, Vector3.up);
-
-        float newAngle = Mathf.LerpAngle(currentAngle, targetAngle, rotationSpeed * Time.deltaTime);
-
-        newAngle = newAngle >= 0 ? 
-            Mathf.Clamp(Mathf.Abs(newAngle), minAbsAngle, maxAbsAngle) : 
-            -Mathf.Clamp(Mathf.Abs(newAngle), minAbsAngle, maxAbsAngle);
-
+        float newAngle = _rotationCalculator.CalculateRotation(transform.rotation.eulerAngles.y, _preDirection);
+        
         transform.rotation = Quaternion.Euler(0f, newAngle, 0f);
     }
 }
