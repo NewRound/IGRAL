ecc7823de8e60ab4421a1d3258183e19ddec0a54
kim ju hyung <kim52116@naver.com>
2023-10-26 18:31:02 +0900

    [Update] 아이템 등급 설정

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -3,18 +3,19 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     public enum Type { Weapon, Artifact, Consumable, ingredient, ETC}
+    public enum Rarity { Normal, Rare, Unique, Epic}
 
     [field: Header("# Base Info")]
     [field: SerializeField] public string ItemName { get; private set;}
     [field: SerializeField] public Type ItemType { get; private set; }
+    [field: SerializeField] public Rarity ItemRarity { get; private set; }
+    [field: SerializeField] public float DropProbability { get; private set; }
     [field: SerializeField] public Sprite ItemIcon { get; private set;}
     [field: SerializeField] public GameObject ItemObject { get; private set; }
     [field :TextArea][field: SerializeField] public string ItemInfo { get; private set;}
-    [field: SerializeField] public float DropProbability { get; private set; } 
     [field: SerializeField] public int buyPrice { get; private set;}
     [field: SerializeField] public int sellPrice { get; private set;}
     [field: SerializeField] public bool isStackable { get; private set;}
     [field: SerializeField] public int maxStack { get; private set;}
-    [field: SerializeField] public PlayerSO itemData { get; private set; }
-       
+    [field: SerializeField] public PlayerSO itemData { get; private set; }       
 }
