567c5ca344c15cae71af6f844f2e3f5aafae929a
kim ju hyung <kim52116@naver.com>
2023-10-26 17:39:00 +0900

    [Add] 아이템 제작
    테스트용 아이템
    
    무기 3개
    소모 1개
    유물 1개

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -2,18 +2,19 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+    public enum Type { Weapon, Artifact, Consumable, ingredient, ETC}
+
     [field: Header("# Base Info")]
     [field: SerializeField] public string ItemName { get; private set;}
-    [field: SerializeField] public int ItemID { get; private set;}
+    [field: SerializeField] public Type ItemType { get; private set; }
     [field: SerializeField] public Sprite ItemIcon { get; private set;}
     [field: SerializeField] public GameObject ItemObject { get; private set; }
     [field :TextArea][field: SerializeField] public string ItemInfo { get; private set;}
+    [field: SerializeField] public float DropProbability { get; private set; } 
     [field: SerializeField] public int buyPrice { get; private set;}
     [field: SerializeField] public int sellPrice { get; private set;}
     [field: SerializeField] public bool isStackable { get; private set;}
     [field: SerializeField] public int maxStack { get; private set;}
-    [field: SerializeField] public bool isEquipment { get; private set; }
-    [field: SerializeField] public bool isConsumable { get; private set; }
     [field: SerializeField] public PlayerSO itemData { get; private set; }
        
 }
