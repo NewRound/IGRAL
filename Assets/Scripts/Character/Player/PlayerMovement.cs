d0929721aa9f8f2283ba38be6b1f8a1a6c4619c1
JuChan Jeong <jjc6252498@gmail.com>
2023-10-26 10:35:50 +0900

    [Add] Jump,Slide,Attack Input 틀 추가, [Update] 쓸데 없는 using문 제거

--- Assets/Scripts/Character/Player/PlayerMovement.cs
+++ Assets/Scripts/Character/Player/PlayerMovement.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
@@ -36,6 +33,7 @@ public class PlayerMovement : MonoBehaviour
     private void OnEnable()
     {
         _controller.MoveAction += SetDirection;
+        //_controller.JumpAction +=
     }
 
     private void Start()
@@ -90,5 +88,10 @@ public class PlayerMovement : MonoBehaviour
         float newAngle = _rotationCalculator.CalculateRotation(transform.rotation.eulerAngles.y, _preDirection);
         
         transform.rotation = Quaternion.Euler(0f, newAngle, 0f);
+    }
+
+    private void Jump()
+    {
+
     }
 }
