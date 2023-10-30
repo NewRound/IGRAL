54f7460183754c9ca95bf2d9b511a81ffc6e524a
JuChan Jeong <jjc6252498@gmail.com>
2023-10-30 14:15:19 +0900

    [Update] Input 키보드 조이스틱 투 트랙으로 나누기

--- Assets/Scripts/Character/Player/MovementData.cs
+++ Assets/Scripts/Character/Player/MovementData.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 [Serializable]
 public class MovementData
 {
+    [field : SerializeField] public Transform ModelTrans { get; private set; }
     [field: SerializeField] public float AcceleratingTime { get; private set; } = 1f;
     [field: SerializeField] public float RotationSpeed { get; private set; } = 10f;
     [field: SerializeField] public float MinAbsAngle { get; private set; } = 90f;
