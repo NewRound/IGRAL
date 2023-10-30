b4aadb52533e364b646d741f91574417f32c15c1
NewRound <86642886+NewRound@users.noreply.github.com>
2023-10-30 10:36:20 +0900

    [Update] 변수명 변경

--- Assets/Scripts/Map/MapGenerator.cs
+++ Assets/Scripts/Map/MapGenerator.cs
@@ -1,23 +1,21 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
-using static Unity.Collections.AllocatorManager;
 
 public class MapGenerator : MonoBehaviour
 {
     public static MapGenerator Instance;
 
-    [SerializeField] public List<GameObject> Stage1_Blocks;
-    [SerializeField] public List<GameObject> Stage2_Blocks;
-    [SerializeField] public List<GameObject> Stage3_Blocks;
+    [SerializeField] public List<GameObject> Stage1_mapBlocks;
+    [SerializeField] public List<GameObject> Stage2_mapBlocks;
+    [SerializeField] public List<GameObject> Stage3_mapBlocks;
 
-    [SerializeField] public int numberOfBlock;
+    [SerializeField] public int numberOfmapBlock;
     [SerializeField] public int lastStage = 3;
     [SerializeField] public int tileSize;
 
-    private GameObject frontBlock = null;
+    private GameObject frontmapBlock = null;
 
-    private List<int> RandomBlockIndex = new List<int>();
+    private List<int> RandommapBlockIndex = new List<int>();
 
     private int currentStage = 0;
 
@@ -47,13 +45,13 @@ public class MapGenerator : MonoBehaviour
         switch(currentStage)
         {
             case 0:
-                createStage = Stage1_Blocks;
+                createStage = Stage1_mapBlocks;
                 break;
             case 1:
-                createStage = Stage2_Blocks;
+                createStage = Stage2_mapBlocks;
                 break;
             case 2:
-                createStage = Stage3_Blocks;
+                createStage = Stage3_mapBlocks;
                 break;
             default:
                 createStage = null;
@@ -63,21 +61,21 @@ public class MapGenerator : MonoBehaviour
         if(createStage == null || createStage.Count == 0)
             return;
 
-        Debug.Log($"{numberOfBlock}, {createStage.Count - 2} ");
+        Debug.Log($"{numberOfmapBlock}, {createStage.Count - 2} ");
 
         // 랜덤 생성 TODO
-        if (createStage.Count - 2 < numberOfBlock)
+        if (createStage.Count - 2 < numberOfmapBlock)
         {
             int randomValue;
-            Debug.Log($"{RandomBlockIndex.Count}, {createStage.Count - 2} ");
+            Debug.Log($"{RandommapBlockIndex.Count}, {createStage.Count - 2} ");
 
-            while (RandomBlockIndex.Count != createStage.Count - 2)
+            while (RandommapBlockIndex.Count != createStage.Count - 2)
             {
                 randomValue = Random.Range(1, createStage.Count - 1);
 
-                if (!RandomBlockIndex.Contains(randomValue))
+                if (!RandommapBlockIndex.Contains(randomValue))
                 {
-                    RandomBlockIndex.Add(randomValue);
+                    RandommapBlockIndex.Add(randomValue);
                     Debug.Log(randomValue);
                 }
             }
@@ -87,13 +85,13 @@ public class MapGenerator : MonoBehaviour
             int randomValue;
             
 
-            while (RandomBlockIndex.Count != numberOfBlock)
+            while (RandommapBlockIndex.Count != numberOfmapBlock)
             {
                 randomValue = Random.Range(1, createStage.Count - 1);
 
-                if (!RandomBlockIndex.Contains(randomValue))
+                if (!RandommapBlockIndex.Contains(randomValue))
                 {
-                    RandomBlockIndex.Add(randomValue);
+                    RandommapBlockIndex.Add(randomValue);
                 }
             }
         }
@@ -102,9 +100,9 @@ public class MapGenerator : MonoBehaviour
         // 첫맵 생성
         CreateBlock(createStage, 0);
 
-        for (int i = 0; i < RandomBlockIndex.Count; i++)
+        for (int i = 0; i < RandommapBlockIndex.Count; i++)
         {
-            CreateBlock(createStage, RandomBlockIndex[i]);
+            CreateBlock(createStage, RandommapBlockIndex[i]);
         }
 
         // 마지막 맵 생성
@@ -116,12 +114,12 @@ public class MapGenerator : MonoBehaviour
 
     private void CreateBlock(List<GameObject> _createStage, int createIndex)
     {
-        GameObject Block = Instantiate(_createStage[createIndex]);
-        Vector3 _position = Block.transform.position;
-        if (frontBlock != null)
+        GameObject newBlock = Instantiate(_createStage[createIndex]);
+        Vector3 _position = newBlock.transform.position;
+        if (frontmapBlock != null)
         {
-            Vector3 _frontPosition = frontBlock.transform.position;
-            MapData _mapData = frontBlock.GetComponent<MapData>();
+            Vector3 _frontPosition = frontmapBlock.transform.position;
+            MapData _mapData = frontmapBlock.GetComponent<MapData>();
             _position.x = _frontPosition.x + (_mapData.horizontalOfTiles * tileSize);
             _position.y = _frontPosition.y + (_mapData.verticalOfTiles * tileSize);
         }
@@ -130,33 +128,33 @@ public class MapGenerator : MonoBehaviour
             _position.x = 0;
             _position.y = 0;
         }
-        Block.transform.position = _position;
-        frontBlock = Block;
+        newBlock.transform.position = _position;
+        frontmapBlock = newBlock;
     }
 
 
     // 포지션 계산 함수.
     /*
-    private Vector3 CalculatePosition(GameObject blockPrefab, bool isEnterPosition)
+    private Vector3 CalculatePosition(GameObject mapBlockPrefab, bool isEnterPosition)
     {
         if(isEnterPosition)
         {
-            Transform value = blockPrefab.transform.Find("EnterPosition");
+            Transform value = mapBlockPrefab.transform.Find("EnterPosition");
 
             if (value != null)
             {
-                Vector3 answer = blockPrefab.transform.position + value.position;
+                Vector3 answer = mapBlockPrefab.transform.position + value.position;
 
                 return answer;
             }
         }
         else
         {
-            Transform value = blockPrefab.transform.Find("ExitPosition");
+            Transform value = mapBlockPrefab.transform.Find("ExitPosition");
 
             if (value != null)
             {
-                Vector3 answer = blockPrefab.transform.position + value.position;
+                Vector3 answer = mapBlockPrefab.transform.position + value.position;
 
                 return answer;
             }
