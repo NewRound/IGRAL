4a37c9e6962d0248651ad7e7b6a781ac60364c91
한병권 <hbg9212@gmail.com>
2023-10-30 10:58:52 +0900

    [Add] 배경음 임시 추가

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum AudioType
+{
+    BGMAudio
+}
+
+public class AudioManager : CustomSingleton<AudioManager>
+{
+    private float _bgmVolume;
+    private float _sfxVolume;
+
+    private AudioSource bgmPlayer;
+    private AudioSource[] sfxPlayer;
+
+    private void Awake()
+    {
+        foreach (AudioType enumItem in Enum.GetValues(typeof(AudioType)))
+        {
+            GameObject ui = Resources.Load<GameObject>($"Audio/{GetDescription.EnumToString(enumItem)}");
+            GameObject instantiate = Instantiate(ui, Vector3.zero, Quaternion.identity);
+            instantiate.transform.SetParent(this.transform);
+        }
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+
+    }
+}
