8325d9bb69360f5a7dfe9f8ed4a6b0e1ad650681
kim ju hyung <kim52116@naver.com>
2023-11-01 13:05:10 +0900

    [Add] EquipManager
    플레이어 프리팹에 장착된 무기
    키거나 끄는 기능

---
+++ Assets/Scripts/Item/EquipManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class EquipManager : CustomSingleton<GameManager>
+{
+    [SerializeField] private GameObject backEquip;
+    [SerializeField] private GameObject HandEquip;       
+
+    public void SetBackWeapon()
+    {
+        backEquip.SetActive(true);
+        HandEquip.SetActive(false);        
+    }
+
+    public void SetHandWeapon()
+    {
+        backEquip.SetActive(false);
+        HandEquip.SetActive(true);
+    }
+}
