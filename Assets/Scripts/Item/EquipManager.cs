0dae54221e7ed89171ada774b58d7151d68c325c
kim ju hyung <kim52116@naver.com>
2023-11-01 19:38:05 +0900

    [Add] 장비스탯 구상
    초기 구상안

--- Assets/Scripts/Item/EquipManager.cs
+++ Assets/Scripts/Item/EquipManager.cs
@@ -1,9 +1,30 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EquipManager : CustomSingleton<EquipManager>
 {
     [SerializeField] private GameObject backWeaponPos;
-    [SerializeField] private GameObject HandWeaponPos;       
+    [SerializeField] private GameObject HandWeaponPos;
+
+    private List<Item> curEquipments;
+
+    public PlayerSO EquipmentStat()
+    {
+        // 인벤토리에서 현재 장착된 것들을 가져오기
+        // curEquipments = ???
+
+        // 능력치들을 다 더한 장비스탯 만들기
+        PlayerSO equipmentSO = new PlayerSO();
+
+       
+        foreach (var item in curEquipments)
+        {
+            // curEquipments 들의 능력치를 합산하기
+            // equipmentSO 에 합산한 능력치 할당?
+        }
+
+        return equipmentSO;
+    }
 
     public void SetBackWeapon()
     {
