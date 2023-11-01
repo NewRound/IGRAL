70aa662bf7bd60a90d68a1373ab2533da96817bd
NewRound <86642886+NewRound@users.noreply.github.com>
2023-11-01 20:51:26 +0900

    [Add] Area가 설정된 발판 생성, [Update] TestMap 프리펩에 적을 생성할 수 있게 적용

--- Assets/Scripts/Map/EnemyGenerator.cs
+++ Assets/Scripts/Map/EnemyGenerator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyGenerator : MonoBehaviour
 {
-    [SerializeField] private List<Vector3> SpawnPoints;
+    [SerializeField] private List<Transform> SpawnPoints;
     [SerializeField] private List<GameObject> Enemys;
 
     private void Awake()
@@ -14,12 +14,12 @@ public class EnemyGenerator : MonoBehaviour
 
     private void Start()
     {
-        foreach(Vector3 position in SpawnPoints)
+        foreach(Transform position in SpawnPoints)
         {
             int randomValue = Random.Range(0, Enemys.Count);
             GameObject enemy = Instantiate(Enemys[randomValue]);
 
-            enemy.transform.position = position;
+            enemy.transform.position = position.position;
         }
     }
 }
