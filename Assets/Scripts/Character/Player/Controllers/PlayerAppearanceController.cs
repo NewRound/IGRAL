efcb42a1cb2b80901d2c26b6af98c74a9fd0d4e0
NewRound <86642886+NewRound@users.noreply.github.com>
2023-11-03 17:39:30 +0900

    [Update] PlayerAppearanceContoller 작동 가능한 1차 완성본, SkillHammer와 SkillKnife에 연동.

--- Assets/Scripts/Character/Player/Controllers/PlayerAppearanceController.cs
+++ Assets/Scripts/Character/Player/Controllers/PlayerAppearanceController.cs
@@ -1,18 +1,98 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum MutantType
+{
+    None,
+    Stone,
+    Blade,
+    Sheld,
+    Skin,
+}
+
 public class PlayerAppearanceController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private MutantType mutantType;
+
+    [SerializeField] public List<GameObject> Mutant_Stone;
+    [SerializeField] public List<GameObject> Mutant_Blade;
+    [SerializeField] public List<GameObject> Mutant_Sheld;
+    [SerializeField] public List<GameObject> Mutant_SkinBaby;
+
+    private void Awake()
     {
-        
+        mutantType = MutantType.None;
+        OnOffMutant(MutantType.Stone, false);
+        OnOffMutant(MutantType.Blade, false);
+        OnOffMutant(MutantType.Sheld, false);
+        OnOffMutant(MutantType.Skin, false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeMutant(MutantType type)
     {
+        if(type == MutantType.None)
+        {
+            OnOffMutant(mutantType, false);
+            mutantType = MutantType.None;
+            return;
+        }
+
+        if(mutantType != MutantType.None)
+        {
+            OnOffMutant(mutantType, false);
+        }
+
+        mutantType = type;
+
+        switch (type)
+        {
+            case MutantType.Stone:
+                OnOffMutant(mutantType, true);
+                break;
+            case MutantType.Blade:
+                OnOffMutant(mutantType, true);
+                break;
+            case MutantType.Sheld:
+                OnOffMutant(mutantType, true);
+                break;
+            case MutantType.Skin:
+                OnOffMutant(mutantType, true);
+                break;
+            default:
+                return;
+        }
+    }
+
+    private void OnOffMutant(MutantType type, bool OnOff)
+    {
+        List<GameObject> Mutant;
+        switch (type)
+        {
+            case MutantType.Stone:
+                Mutant = Mutant_Stone;
+
+                break;
+            case MutantType.Blade:
+                Mutant = Mutant_Blade;
+
+                break;
+            case MutantType.Sheld:
+                Mutant = Mutant_Sheld;
+
+                break;
+            case MutantType.Skin:
+                Mutant = Mutant_SkinBaby;
+
+                break;
+            default:
+                return;
+        }
+        foreach (GameObject obj in Mutant)
+        {
+            obj.SetActive(OnOff);
+        }
         
     }
+    
 }
