9d0a23299e1a957f220074a46fe6d9e42ac0203b
kim ju hyung <kim52116@naver.com>
2023-11-03 19:46:37 +0900

    [Add] 드론 총알 구상

--- Assets/Scripts/Drone/DroneProjectile.cs
+++ Assets/Scripts/Drone/DroneProjectile.cs
@@ -2,15 +2,38 @@ using UnityEngine;
 
 public class DroneProjectile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float _attackDamage;
+    private Vector3 _myPos;
+    private Vector3 _targetPos;
+
+    private Rigidbody _rigid;
+
+    public DroneProjectile(Vector3 tagetPos, float attackDamage)
     {
-        
+        _targetPos = tagetPos;
+        this._attackDamage = attackDamage;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        _rigid = GetComponent<Rigidbody>();
+    }    
+
+    private void OnEnable()
+    {
+        _myPos = transform.position;
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy") 
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
