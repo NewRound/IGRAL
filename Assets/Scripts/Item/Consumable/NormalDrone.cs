9c1c247e641fa895c25c20f19cca14d890e9f9c9
한병권 <hbg9212@gmail.com>
2023-11-10 14:25:58 +0900

    [Update] 소비형 아이템 드론 배터리 구현 완료

--- Assets/Scripts/Item/Consumable/NormalDrone.cs
+++ Assets/Scripts/Item/Consumable/NormalDrone.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 
 public class NormalDrone : ItemConsumable
 {
+
     public override void UseConsumable()
     {
         base.UseConsumable();
-        Debug.Log("NormalDrone");
+        GameManager.Instance.drone.ActiveDrone(30f);
     }
 }
