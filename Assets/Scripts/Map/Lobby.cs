7b82337a1641848a3dc9fd0e6d21f2a8cc408556
한병권 <hbg9212@gmail.com>
2023-11-14 12:29:06 +0900

    [Fix] 씬 이동시 플레이어 이동 구현

---
+++ Assets/Scripts/Map/Lobby.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Lobby : MonoBehaviour
+{
+    public GameObject Player;
+    public Transform PlayerTransform;
+    public InputController PlayerInputController;
+    public GameObject droneGo;
+
+}
