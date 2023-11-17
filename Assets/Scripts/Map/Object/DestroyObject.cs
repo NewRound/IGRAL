8aaa9af708f143730a1324ff04f41383453d6f1c
kim ju hyung <kim52116@naver.com>
2023-11-17 20:31:18 +0900

    [Update] 파괴오브젝트 구현중

--- Assets/Scripts/Map/Object/DestroyObject.cs
+++ Assets/Scripts/Map/Object/DestroyObject.cs
@@ -1,7 +1,8 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class DestroyObject : MonoBehaviour
+public class DestroyObject : MonoBehaviour, IInteract
 {
     [SerializeField] private GameObject _createObj;
     [SerializeField] private LayerMask _canBeDestroyBy;
@@ -19,24 +20,19 @@ public class DestroyObject : MonoBehaviour
         _audioSource = GetComponent<AudioSource>();
         _audioClip = _audioSource.clip;
         _rigids = new List<Rigidbody>();
-    }    
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (!(_canBeDestroyBy.value == ( _canBeDestroyBy.value | (1 << collision.gameObject.layer)))) return;
-        
-        if (CheckMutantType())
-        {
-            DestroyObj();            
-            CreateObj();
-        }        
-    }   
+    }
 
     // 문제점
-    // OnCollsionEnter > 오류, 망치상태로 공격을 하지 않아도 가까이 가면 작동 
-    // 공격을 할 때만 작동하게 하는 방법?
+    // 새로운 객체가 생기면서 플레이어가 갇힘
 
-    // 바닥이 생성되면서 플레이어가 갇혀버림
+    public void Interact()
+    {
+        if (_player.mutantType != MutantType.Stone) return;
+
+        Debug.Log("공격받음");
+        DestroyObj();
+        CreateObj();
+    }
 
     private void DestroyObj()
     {
@@ -73,10 +69,10 @@ public class DestroyObject : MonoBehaviour
         Instantiate(_createObj, transform.position + new Vector3(-5f, -2f, 0f), Quaternion.identity);
     }
 
-    private bool CheckMutantType()
-    {
-        if (_player.mutantType == MutantType.Stone) return true;
+    //private bool CheckMutantType()
+    //{
+    //    if (_player.mutantType == MutantType.Stone) return true;
 
-        return false;
-    }
+    //    return false;
+    //}   
 }
