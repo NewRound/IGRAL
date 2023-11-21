dadd9852975081f6fb51bab8b49c14903a7b07a3
NewRound <86642886+NewRound@users.noreply.github.com>
2023-11-21 14:26:09 +0900

    [Update] 튜토맵 1차 제작 완료

---
+++ Assets/Scripts/Map/Object/TurretObject.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class TurretObject : MonoBehaviour
+{
+    [SerializeField] private Transform spawnPoint;    
+    [SerializeField] private GameObject bullet;
+    public Vector2 Direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
