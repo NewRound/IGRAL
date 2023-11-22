7e752b16d84fd1cc6b4fb417ab92449c89c979cc
NewRound <86642886+NewRound@users.noreply.github.com>
2023-11-22 14:47:43 +0900

    [Update] 터렛 제작 중...

---
+++ Assets/Scripts/Map/Object/TurretBullet.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class TurretBullet : MonoBehaviour
+{
+    [SerializeField] private LayerMask _targetLayer;
+
+    [SerializeField] private float _attackDamage;
+    [SerializeField] private float _movementSpeed;
+    [SerializeField] private float _maxDuration;
+    private float _curDuration;
+    private Vector3 _direction = Vector3.zero;
+
+    private void OnEnable()
+    {
+        _curDuration = 0f;
+    }
+
+    private void OnDisable()
+    {
+        _direction = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if (_curDuration >= _maxDuration)
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (_direction != Vector3.zero)
+        {
+            transform.forward = _direction.normalized;
+            transform.Translate(Vector3.forward * _movementSpeed * Time.deltaTime);
+        }
+        _curDuration += Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        InputController inputController = other.GetComponent<InputController>();
+
+        if (inputController != null)
+        {
+            PlayerAppearanceController playerAppearanceController = other.GetComponent<PlayerAppearanceController>();
+
+            if(playerAppearanceController.mutantType != MutantType.Sheld)
+            {
+                PlayerStatHandler player = other.GetComponent<PlayerController>().StatHandler;
+                if (player != null)
+                {
+                    player.Damaged(_attackDamage);
+                }
+                gameObject.SetActive(false);
+            }
+        }
+        else if(_targetLayer == (1 << other.gameObject.layer))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
