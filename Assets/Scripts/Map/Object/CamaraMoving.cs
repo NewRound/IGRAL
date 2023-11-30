a3108f4265f4e59745262d1e094f2980632deac5
NewRound <86642886+NewRound@users.noreply.github.com>
2023-11-30 20:56:32 +0900

    [Add] 맵의 연출을 위해 작업을 진행중.

---
+++ Assets/Scripts/Map/Object/CamaraMoving.cs
@@ -0,0 +1,13 @@
+using Cinemachine;
+using UnityEngine;
+
+public class CamaraMoving : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera cam;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/Map/Object/EnemyContainerSpawner.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyContainerSpawner : MonoBehaviour, IObject
+{
+    [SerializeField] private Transform SpawnPoint;
+    [SerializeField] private GameObject LeftDoor;
+    [SerializeField] private GameObject RightDoor;
+    [SerializeField] private float rotateEngle;
+
+    public float speed;
+    public int enemyCount;
+
+
+    private List<GameObject> enemys;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemys = new List<GameObject>();
+    }
+
+
+    public void Use()
+    {
+        for(int i = 0; i < enemyCount; i++)
+        {
+            // 에너미 풀에서 몬스터 가져오기.
+            // 에너미 위치 조정.
+            // 대기.
+        }
+
+        StartCoroutine(rotateDoor(rotateEngle));
+
+        // 적들을 축으로 이동시키고 이에따라 Area에 편입.
+    }
+
+    IEnumerator rotateDoor(float engle)
+    {
+        float curEngle = 0.0f;
+
+        while (true)
+        {
+            LeftDoor.transform.rotation *= Quaternion.Euler(0, Time.deltaTime * speed, 0);
+            RightDoor.transform.rotation *= Quaternion.Euler(0, -Time.deltaTime * speed, 0);
+            curEngle += Time.deltaTime * speed;
+
+            Debug.Log(curEngle);
+
+            if (curEngle > engle)
+                break;
+
+            yield return 0;
+        }
+    }
+
+}
