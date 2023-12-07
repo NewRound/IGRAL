be33195c7f552e0257360e2f669b5103936a981c
kim ju hyung <kim52116@naver.com>
2023-12-07 12:07:32 +0900

    [Update] skillBlink 추가

--- Assets/Scripts/UI/UITopButton.cs
+++ Assets/Scripts/UI/UITopButton.cs
@@ -7,11 +7,41 @@ public class UITopButton : MonoBehaviour
     [SerializeField] private Button _inventory;
     [SerializeField] private Button _skillTree;
 
+    private Color _alpha;
+    public bool _skillTreeBlink;
+    private float _blinkSpeed;
+    private float _blinkTime;
+
     private void Awake()
     {
         _option.onClick.AddListener(OpenOption);
         _inventory.onClick.AddListener(OpenInventory);
         _skillTree.onClick.AddListener(OpenSkillTree);
+
+        _alpha = _skillTree.image.color;
+        _blinkSpeed = 5f;
+    }
+
+    private void Update()
+    {
+        _blinkTime += Time.deltaTime;
+
+        if (_skillTreeBlink)
+        {
+            _alpha.a = (Mathf.Cos(_blinkTime * _blinkSpeed) + 1) * GlobalValues.HALF;
+            _skillTree.image.color = _alpha;
+        }
+
+        else
+        {
+            AlphaInit();
+        }
+    }
+
+    private void AlphaInit()
+    {
+        _alpha.a = 1f;
+        _skillTree.image.color = _alpha;
     }
 
     private void OpenOption()
